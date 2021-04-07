ac612e208ea5c7918758df3ae9b177d4c6759f54
Yongwook Choi <yongwook.choi@riiid.co>
2021-04-07 10:46:16 +0900

    feat(zigzag): Implement zigzag encoding

--- core/runtime/wire/zigzag.test.ts
+++ core/runtime/wire/zigzag.test.ts
@@ -1,4 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.84.0/testing/asserts.ts";
+import Long from "../Long.ts";
 import zigzag from "./zigzag.ts";
 
 Deno.test("zigzag", () => {
@@ -7,4 +8,7 @@ Deno.test("zigzag", () => {
   assertEquals(zigzag(1), 2);
   assertEquals(zigzag(-2), 3);
   assertEquals(zigzag(2147483647), 4294967294);
+  assertEquals(zigzag(-2147483648), 4294967295);
+  assertEquals(zigzag(Long.parse("2147483647")), Long.parse("4294967294"));
+  assertEquals(zigzag(Long.parse("-2147483648")), Long.parse("4294967295"));
 });
--- core/runtime/wire/zigzag.ts
+++ core/runtime/wire/zigzag.ts
@@ -1,5 +1,13 @@
 import Long from "../Long.ts";
 
 export default function zigzag(value: number | Long): number | Long {
-  throw "TODO";
+  if (value instanceof Long) {
+    const l = new Long(
+      value[0] << 1,
+      (value[1] << 1) | (value[0] >>> 31),
+    );
+    const r = value[1] >>> 31 ? new Long(0xFFFFFFFF, 0xFFFFFFFF) : new Long();
+    return new Long(l[0] ^ r[0], l[1] ^ r[1]);
+  }
+  return ((value * 2) ^ (value >> 31)) >>> 0;
 }
