1f7ef50bb338d8eb1b6b1b092ffcd3f0a1b0a307
Yongwook Choi <yongwook.choi@riiid.co>
2021-04-07 15:25:20 +0900

    feat(zigzag): Implement zigzag decoder

--- core/runtime/wire/zigzag.ts
+++ core/runtime/wire/zigzag.ts
@@ -1,6 +1,6 @@
 import Long from "../Long.ts";
 
-export default function zigzag(value: number | Long): number | Long {
+export function encode(value: number | Long): number | Long {
   if (value instanceof Long) {
     const l = new Long(
       value[0] << 1,
@@ -10,4 +10,13 @@ export default function zigzag(value: number | Long): number | Long {
     return new Long(l[0] ^ r[0], l[1] ^ r[1]);
   }
   return ((value * 2) ^ (value >> 31)) >>> 0;
+}
+
+export function decode(value: number | Long): number | Long {
+  if (value instanceof Long) {
+    const l = new Long((value[0] >>> 1) | (value[1] << 31), (value[1]) >>> 1);
+    const r = value[0] & 1 ? new Long(0xFFFFFFFF, 0xFFFFFFFF) : new Long();
+    return new Long(l[0] ^ r[0], l[1] ^ r[1]);
+  }
+  return (value >>> 1) ^ -(value & 1);
 }
