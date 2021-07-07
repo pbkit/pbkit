cbf9959c3b3a90870236f448922d4440fac99611
BeLeap <changseo_jang@korea.ac.kr>
2021-07-07 16:56:06 +0900

    remove await imports

--- cli/pollapo/entrypoint.ts
+++ cli/pollapo/entrypoint.ts
@@ -1,12 +1,18 @@
 import { Command } from "https://deno.land/x/cliffy@v0.19.1/command/mod.ts";
 
+import Add from "./cmds/add.ts";
+import Install from "./cmds/install.ts";
+import Login from "./cmds/login.ts";
+import Remove from "./cmds/remove.ts";
+import Why from "./cmds/why.ts";
+
 const command = new Command();
 command
   .name("pollapo")
   .arguments("<command> [options]")
-  .command("add", (await import("./cmds/add.ts")).default)
-  .command("install", (await import("./cmds/install.ts")).default)
-  .command("login", (await import("./cmds/login.ts")).default)
-  .command("remove", (await import("./cmds/remove.ts")).default)
-  .command("why", (await import("./cmds/why.ts")).default)
+  .command("add", Add)
+  .command("install", Install)
+  .command("login", Login)
+  .command("remove", Remove)
+  .command("why", Why)
   .parse(Deno.args);
