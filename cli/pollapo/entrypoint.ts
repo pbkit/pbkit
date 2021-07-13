c5c00e5da75d1f199d82736c50001cc9fa52e61e
BeLeap <changseo_jang@korea.ac.kr>
2021-07-13 15:10:34 +0900

    fix PascalCase import

--- cli/pollapo/entrypoint.ts
+++ cli/pollapo/entrypoint.ts
@@ -1,18 +1,18 @@
 import { Command } from "https://deno.land/x/cliffy@v0.19.1/command/mod.ts";
 
-import Add from "./cmds/add.ts";
-import Install from "./cmds/install.ts";
-import Login from "./cmds/login.ts";
-import Remove from "./cmds/remove.ts";
-import Why from "./cmds/why.ts";
+import add from "./cmds/add.ts";
+import install from "./cmds/install.ts";
+import login from "./cmds/login.ts";
+import remove from "./cmds/remove.ts";
+import why from "./cmds/why.ts";
 
 const command = new Command();
 command
   .name("pollapo")
   .arguments("<command> [options]")
-  .command("add", Add)
-  .command("install", Install)
-  .command("login", Login)
-  .command("remove", Remove)
-  .command("why", Why)
+  .command("add", add)
+  .command("install", install)
+  .command("login", login)
+  .command("remove", remove)
+  .command("why", why)
   .parse(Deno.args);
