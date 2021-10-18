d436aa76a5f022ec583e35ada7578208976e02eb
JongChan Choi <jong@chan.moe>
2021-10-17 18:13:12 -0700

    fix #114

---
+++ misc/case.test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std@0.107.0/testing/asserts.ts";
+import { snakeToCamel } from "./case.ts";
+
+Deno.test("snakeToCamel", () => {
+  assertEquals(snakeToCamel("foo"), "foo");
+  assertEquals(snakeToCamel("foo_bar"), "fooBar");
+  assertEquals(snakeToCamel("foo_bar_baz"), "fooBarBaz");
+  assertEquals(snakeToCamel("foo__bar"), "fooBar");
+  assertEquals(snakeToCamel(""), "");
+  assertEquals(snakeToCamel("_"), "_");
+  assertEquals(snakeToCamel("_foo"), "_foo");
+  assertEquals(snakeToCamel("__foo"), "__foo");
+  assertEquals(snakeToCamel("foo_"), "foo_");
+  assertEquals(snakeToCamel("foo__"), "foo__");
+  assertEquals(snakeToCamel("_foo_bar"), "_fooBar");
+  assertEquals(snakeToCamel("foo_bar_"), "fooBar_");
+  assertEquals(snakeToCamel("_foo_bar_"), "_fooBar_");
+});
