8f1f219d8549140e138c23fc80b5cf7a395fb447
JongChan Choi <jong@chan.moe>
2021-10-17 21:54:42 -0700

    Add `pb vendor` command

---
+++ cli/pb/cmds/vendor/cmds/install.ts
@@ -0,0 +1,59 @@
+import { Command } from "https://deno.land/x/cliffy@v0.19.5/command/mod.ts";
+import { ensureDir } from "https://deno.land/std@0.107.0/fs/mod.ts";
+import * as path from "https://deno.land/std@0.107.0/path/mod.ts";
+import { iterFiles } from "../../../../../misc/archive/zip.ts";
+import { getVendorDir } from "../../../config.ts";
+import downloadRepoAndExtractProtoFiles from "../downloadRepoAndExtractProtoFiles.ts";
+
+interface Options {
+  protobufOnly?: boolean;
+  quiet?: boolean;
+}
+
+export default new Command()
+  .option(
+    "--protobuf-only",
+    "Install only proto files of github:protocolbuffers/protobuf.zip.",
+  )
+  .option(
+    "--quiet",
+    "Skip printing log messages.",
+  )
+  .description("Install vendor proto files.")
+  .action(async (options: Options) => {
+    const { protobufOnly = false, quiet = false } = options;
+    const vendorDir = getVendorDir();
+    const protoFiles = iterProtoFiles(protobufOnly, quiet);
+    for await (const { fileName, data } of protoFiles) {
+      const savePath = path.resolve(vendorDir, fileName);
+      if (!quiet) console.log(`Writing ${savePath}...`);
+      await ensureDir(path.dirname(savePath));
+      await Deno.writeFile(savePath, data);
+    }
+  });
+
+async function* iterProtoFiles(protobufOnly: boolean, quiet: boolean) {
+  const verbose = !quiet;
+  yield* iterFiles(
+    await downloadRepoAndExtractProtoFiles({
+      user: "protocolbuffers",
+      repo: "protobuf",
+      rev: "master",
+      filter: (file) =>
+        file.startsWith("google/protobuf/") &&
+        file.endsWith(".proto"),
+      depth: 2,
+      verbose,
+    }),
+  );
+  if (protobufOnly) return;
+  yield* iterFiles(
+    await downloadRepoAndExtractProtoFiles({
+      user: "googleapis",
+      repo: "googleapis",
+      rev: "master",
+      filter: (file) => file.endsWith(".proto"),
+      verbose,
+    }),
+  );
+}
---
+++ cli/pb/cmds/vendor/downloadRepoAndExtractProtoFiles.ts
@@ -0,0 +1,41 @@
+import { green, yellow } from "https://deno.land/std@0.107.0/fmt/colors.ts";
+import ProgressBar from "https://deno.land/x/progress@v1.2.3/mod.ts";
+import { subscribeFnToAsyncGenerator } from "../../../../core/runtime/async/observer.ts";
+import { progressResponse } from "../../../../misc/fetch.ts";
+import { stripComponents, unzip } from "../../../../misc/archive/zip.ts";
+import { fetchArchive } from "../../../../misc/github/index.ts";
+
+export interface DownloadRepoAndExtractProtoFilesConfig {
+  user: string;
+  repo: string;
+  rev: string;
+  filter?: (file: string) => boolean;
+  depth?: number;
+  verbose?: boolean;
+}
+export default async function downloadRepoAndExtractProtoFiles({
+  user,
+  repo,
+  rev,
+  filter = (file) => file.endsWith(".proto"),
+  depth = 1,
+  verbose = true,
+}: DownloadRepoAndExtractProtoFilesConfig) {
+  const { response, subscribeProgress } = progressResponse(
+    await fetchArchive({ type: "zip", user, repo, rev }),
+  );
+  if (verbose) {
+    console.log(green("Downloading"), yellow(`github:${user}/${repo}.zip`));
+  }
+  const progressBar = new ProgressBar();
+  for await (const progress of subscribeFnToAsyncGenerator(subscribeProgress)) {
+    progressBar.total = progress.total || 7500000;
+    progressBar.render(progress.current);
+  }
+  const zip = new Uint8Array(await response.arrayBuffer());
+  const files = stripComponents(await unzip(zip), depth);
+  const protoFiles = Object.fromEntries(
+    Object.entries(files).filter(([file]) => filter(file)),
+  );
+  return protoFiles;
+}
