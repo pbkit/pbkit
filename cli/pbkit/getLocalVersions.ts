149e7b2ae1a24343bb7a4188f63f05f7447fbc38
Changseo Jang <beleap@beleap.dev>
2023-10-29 21:09:18 +0900

    feat: handle nightly resolves to latest commit on main branch (#291)

--- cli/pbkit/getLocalVersions.ts
+++ cli/pbkit/getLocalVersions.ts
@@ -1,11 +1,11 @@
-import { compareRev, isSemver } from "../pollapo/rev.ts";
+import { compareRev } from "../pollapo/rev.ts";
 import { getVersionsDir } from "./config.ts";
 
 export default function getLocalVersions(): string[] {
   try {
     const items = Array.from(Deno.readDirSync(getVersionsDir()));
     return items.filter(
-      ({ name, isDirectory }) => isDirectory && isSemver(name),
+      ({ isDirectory }) => isDirectory,
     ).map((item) => item.name).sort(compareRev).reverse();
   } catch {
     return [];
--- cli/pbkit/resolveRev.ts
+++ cli/pbkit/resolveRev.ts
@@ -1,7 +1,20 @@
-import { fetchLatestRelease } from "../../misc/github/index.ts";
+import {
+  fetchLatestCommit,
+  fetchLatestRelease,
+} from "../../misc/github/index.ts";
 
 export default async function resolveRev(version: string) {
-  if (version !== "latest") return version;
-  const res = await fetchLatestRelease({ user: "pbkit", repo: "pbkit" });
-  return res.name;
+  switch (version) {
+    case "latest": {
+      const res = await fetchLatestRelease({ user: "pbkit", repo: "pbkit" });
+      return res.name;
+    }
+    case "nightly": {
+      const res = await fetchLatestCommit({ user: "pbkit", repo: "pbkit" });
+      return res.sha;
+    }
+    default: {
+      return version;
+    }
+  }
 }
